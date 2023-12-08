dc65a23a8750ab5053e58f5b990e46221da0015b
dwijvas <dwij.vasisth@gmail.com>
2023-12-08 04:29:09 -0500

    login UI w/o firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/LoginScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'MessageMe',
       theme: ThemeData.dark(),
-      home: LoginScreen(),
+      home: const LoginScreen(),
     );
   }
 }
--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -1,3 +1,5 @@
+import 'package:finalprojexct/components/button.dart';
+import 'package:finalprojexct/components/textfields.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -8,31 +10,88 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  // text controllers
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black26,
-      body: Column(
-        children: [
-          //logo
-          Icon(
-            Icons.person,
-            size: 80,
-            color: Theme.of(context).colorScheme.inversePrimary,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Icon(
+                Icons.group,
+                size: 80,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+
+              const SizedBox(height: 25),
+
+              //welcome message
+              const Text(
+                'MessageMe',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+
+              const SizedBox(height: 50),
+
+              // email textfield
+              MyTextField(
+                controller: emailController,
+                hintText: "Enter Email...",
+                obscureText: false,
+              ),
+
+              const SizedBox(height: 10),
+
+              // password textfield
+              MyTextField(
+                controller: passwordController,
+                hintText: "Enter Password...",
+                obscureText: true,
+              ),
+
+              const SizedBox(height: 10),
+
+
+              //login button
+              MyButton(
+                text: "Login",
+                onTap: (){}
+              ),
+
+              const SizedBox(height: 20),
+
+              //navigate to signup
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Not Registered Yet?",
+                  ),
+                  const SizedBox(width: 8),
+                  GestureDetector(
+                    onTap: (){},
+                    child: const Text(
+                      "Please Sign Up Here",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
           ),
-
-          const SizedBox(height: 25),
-
-          //welcome back
-
-          //textfield email
-
-          //password
-
-          //login button
-
-          //navigate to signup
-        ],
+        ),
       ),
     );
   }
